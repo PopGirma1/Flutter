4f13e6de8d7c9eb827421e1dc9c23c27f8726d75
PopGirma1 <girmaytadesepop@gmail.com>
2021-12-26 16:24:59 +0300

    add quote class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -13,7 +14,11 @@ class ListExample extends StatefulWidget {
 }
 
 class _ListExampleState extends State<ListExample> {
-  List<String> quotes= ['Abebe is the best runner in ethiopia', 'mola is the most known name in ethiopia', 'samidan is the best musician in ethiopia'];
+  List<Quote> quotes= [
+    Quote(author: "Papa", text: "The greatest glory in living lies not in never falling, but in rising every time we fall."),
+    Quote(author: "Papa", text: "The way to get started is to quit talking and begin doing."),
+    Quote(author: "Papa", text: "Your time is limited, so don't waste it living someone else's life."),
+    Quote(author: "Papa", text: "If life were predictable it would cease to be life, and be without flavor.")];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +33,7 @@ class _ListExampleState extends State<ListExample> {
         padding: const EdgeInsets.all(40.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children:quotes.map((quotes) => Text(quotes)).toList(),
+          children:quotes.map((quotes) => Text('${quotes.text}==>${quotes.author}')).toList(),
         ),
       ),
     );
---
+++ lib/quote.dart
@@ -0,0 +1,5 @@
+class Quote{
+  String text;
+  String author;
+  Quote({required this.author,required this.text});
+}
