230edf8ab1dc0ec1fef2b517265267be6db5eac9
PopGirma1 <girmaytadesepop@gmail.com>
2021-12-26 16:00:47 +0300

    sample list

--- lib/main.dart
+++ lib/main.dart
@@ -2,72 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyApp(),
+      home: ListExample(),
     ));
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ListExample extends StatefulWidget {
+  const ListExample({Key? key}) : super(key: key);
+
+  @override
+  State<ListExample> createState() => _ListExampleState();
+}
+
+class _ListExampleState extends State<ListExample> {
+  List<String> quotes= ['Abebe is the best runner in ethiopia', 'mola is the most known name in ethiopia', 'samidan is the best musician in ethiopia'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFA10505),
       appBar: AppBar(
-        title: const Text("Papa"),
+        title: const Text("List of Quotes"),
         centerTitle: true,
         elevation: 0.0,
-        backgroundColor: const Color(0xFF8E0B34),
+        backgroundColor: Colors.redAccent,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(30.0),
+        padding: const EdgeInsets.all(40.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/pap.jpg'),
-                radius: 40,
-              ),
-            ),
-            Divider(
-              height: 200,
-              color: Colors.grey[800],
-            ),
-            Text(
-              "Name",
-              style: TextStyle(
-                letterSpacing: 2,
-                color: Colors.accents[10],
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(height: 10.0),
-            Text(
-              "Girmay Tadese",
-              style: TextStyle(
-                letterSpacing: 2,
-                color: Colors.amberAccent[200],
-                fontSize: 28,
-              ),
-            ),
-            const SizedBox(height: 10.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.green[100],
-                ),
-                const SizedBox(width: 30),
-                const Text(
-                  "girmaytadese@gmail.com",
-                  style: TextStyle(
-                    letterSpacing: 2,
-                    color: Colors.blueGrey,
-                    fontSize: 20,
-                  ),
-                ),
-              ],
-            ),
-          ],
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children:quotes.map((quotes) => Text(quotes)).toList(),
         ),
       ),
     );
