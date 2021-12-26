07c930dba89bd866dffda7f2780187eb0eb46aa0
PopGirma1 <girmaytadesepop@gmail.com>
2021-12-26 14:01:11 +0300

    sample profile

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() =>runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: MyApp(),
+));
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFA10505),
+
+
+      appBar: AppBar(
+        title: const Text("Papa"),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: const Color(0xFF8E0B34),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/image.jpg"),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 200,
+              color: Colors.grey[800],
+            ),
+            Text(
+              "Name",
+              style: TextStyle(
+                letterSpacing: 2,
+                color: Colors.accents[10],
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              "Girmay Tadese",
+              style: TextStyle(
+                letterSpacing: 2,
+                color: Colors.amberAccent[200],
+                fontSize: 28,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.email,
+                color: Colors.green[100],
+              ),
+              const SizedBox(width: 30),
+              const Text(
+                "girmaytadese@gmail.com",
+                style: TextStyle(
+                  letterSpacing: 2,
+                  color: Colors.blueGrey,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+          ],
+        ),
+      ),
+    );
+  }
+}
