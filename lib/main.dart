3708ce43891a5915e24f0f8c89e06ff0a8a98cc2
PopGirma1 <girmaytadesepop@gmail.com>
2021-12-26 19:22:35 +0300

    add delete action on each list item

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class _ListExampleState extends State<ListExample> {
     Quote(author: "Papa", text: "Your time is limited, so don't waste it living someone else's life."),
     Quote(author: "Papa", text: "If life were predictable it would cease to be life, and be without flavor.")];
 
-  Widget quoteStyle(quote){
+  Widget quoteStyle(quote,{required dynamic Function() delete}){
     return Card(
       margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
       child: Padding(
@@ -43,6 +43,11 @@ class _ListExampleState extends State<ListExample> {
                fontSize: 14.0,
                color: Colors.grey[800]
              ),
+           ),
+           const SizedBox(height: 5),
+           FlatButton.icon(onPressed: delete,
+             label: const Text("delete quote"),
+             icon: const Icon(Icons.delete),
            )
          ],
         ),
@@ -60,10 +65,17 @@ class _ListExampleState extends State<ListExample> {
         backgroundColor: Colors.redAccent,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(40.0),
+        padding: const EdgeInsets.all(14.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children:quotes.map((quotes) => quoteStyle(quotes)).toList(),
+          children:quotes.map((quote) => quoteStyle(
+              quote,
+              delete:(){
+                setState(() {
+                  quotes.remove(quote);
+                });
+              }
+          )).toList(),
         ),
       ),
     );
