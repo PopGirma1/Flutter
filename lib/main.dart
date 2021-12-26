f0ff5818e8dbede3a8548237952ab82f9805894d
PopGirma1 <girmaytadesepop@gmail.com>
2021-12-26 18:40:31 +0300

    add card for each list

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,40 @@ class _ListExampleState extends State<ListExample> {
     Quote(author: "Papa", text: "The way to get started is to quit talking and begin doing."),
     Quote(author: "Papa", text: "Your time is limited, so don't waste it living someone else's life."),
     Quote(author: "Papa", text: "If life were predictable it would cease to be life, and be without flavor.")];
+
+  Widget quoteStyle(quote){
+    return Card(
+      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+         children:<Widget> [
+           Text(
+             quote.text,
+             style: TextStyle(
+               fontSize: 18.0,
+               color: Colors.grey[600]
+             ),
+           ),
+           const SizedBox(height: 6.0),
+
+           Text(
+             quote.author,
+             style: TextStyle(
+               fontSize: 14.0,
+               color: Colors.grey[800]
+             ),
+           )
+         ],
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFA10505),
+      backgroundColor: const Color(0xFF574E4E),
       appBar: AppBar(
         title: const Text("List of Quotes"),
         centerTitle: true,
@@ -33,7 +63,7 @@ class _ListExampleState extends State<ListExample> {
         padding: const EdgeInsets.all(40.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children:quotes.map((quotes) => Text('${quotes.text}==>${quotes.author}')).toList(),
+          children:quotes.map((quotes) => quoteStyle(quotes)).toList(),
         ),
       ),
     );
